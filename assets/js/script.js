1f54d1a3f6c36e1e420592aa5f34a70ea2c65d70
Matthew Damron <mattdamron@msn.com>
2020-11-18 17:54:57 -0600

    add task change of status handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // find the #page-content id and assign it var pageContentEl
 var pageContentEl = document.querySelector("#page-content");
 
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
 // function to create task elements
 var taskFormHandler = function(event) {
     // stop the web page from refreshing on its on
@@ -178,8 +181,33 @@ var conpleteEditTask = function(taskNameInput, taskTypeInput, taskId) {
     document.querySelector("#save-task").textContent = "Add Task";
 };
 
+var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    // get the curently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // add selected value to selected task
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+}
+
 // addEventListener to buttonEl and run taskFormHandler
 formEl.addEventListener("submit", taskFormHandler);
 
 // addEventListener to main section for clik
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+// addEventListener to change and run taskStatusChangeHandler
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
