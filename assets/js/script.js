d9a415fcb334f4178fe5cbd5c4403ef1f6c6b96b
Matt Damron <mattdamron@msn.com>
2020-11-19 11:45:59 -0700

    add the drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,12 +212,34 @@ var dragTaskHandler = function(event) {
     console.log("getId:", getId, typeof getId);
 }
 
-var dropZoneDragHandler= function(event) {
+var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        console.dir(taskListEl);
     }
+}
+
+var dropTaskHandler = function(event) {
+    event.preventDefault();
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    } 
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    } 
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    }
+
+    dropZoneEl.appendChild(draggableElement);
 }
 
 // addEventListener to buttonEl and run taskFormHandler
@@ -234,3 +256,6 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 
 // addEventListener to add dragover zones to the task status
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+// addEventListener to drop task into new task status
+pageContentEl.addEventListener("drop", dropTaskHandler);
